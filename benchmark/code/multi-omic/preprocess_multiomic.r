8635d9de37ba9b5322ee7bf865f1127027783d01
unknown <joselb1993@gmail.com>
2022-06-22 17:30:55 +0200

    update

--- benchmark/code/multi-omic/preprocess_multiomic.r
+++ benchmark/code/multi-omic/preprocess_multiomic.r
@@ -11,12 +11,12 @@ if (cesga == T) {
 # Transcriptomics
 # ===
 # GSE117931
-assay = fread('benchmark/extdata/multi-omic/GSE117931/ADEx_data/expression.tsv',
+assay = fread('benchmark/extdata/multi-omic/GSE117931/expression.tsv',
               header = T, data.table = F)
 rownames(assay) = assay[,1]; assay = assay[, -1]
 
 meta = read.delim2(
-  'benchmark/extdata/multi-omic/GSE117931/ADEx_data/metadata.tsv',
+  'benchmark/extdata/multi-omic/GSE117931/metadata.tsv',
   header = T)
 meta = meta[match(colnames(assay), meta$Sample),]
 rownames(meta) = meta$Sample
@@ -33,7 +33,7 @@ saveRDS(eset1.f, file = 'benchmark/data/multi-omic/eset_GSE117931_expression_fil
 
 
 # GSE82221
-assay = fread('benchmark/extdata/multi-omic/GSE82221/ADEx_data/expression.tsv',
+assay = fread('benchmark/extdata/multi-omic/GSE82221/expression.tsv',
               header = T, data.table = F)
 rownames(assay) = assay[,1]; assay = assay[, -1]
 
@@ -58,13 +58,13 @@ saveRDS(eset1.f, file = 'benchmark/data/multi-omic/eset_GSE82221_expression_filt
 # Epigenomics
 # ===
 # GSE117931
-assay = fread('benchmark/extdata/multi-omic/GSE117931/ADEx_data/methylation.tsv',
+assay = fread('benchmark/extdata/multi-omic/GSE117931/methylation.tsv',
               header = T, data.table = F)
 rownames(assay) = assay[,1]; assay = assay[, -1]
 assay = na.omit(assay)
 assay = as.data.frame(t(apply(assay, 1, function(x) log2(x / (1-x)))))
 meta = read.delim2(
-  'benchmark/extdata/multi-omic/GSE117931/ADEx_data/metadata.tsv',
+  'benchmark/extdata/multi-omic/GSE117931/metadata.tsv',
   header = T)
 meta = meta[match(colnames(assay), meta$Sample),]
 rownames(meta) = meta$Sample
@@ -82,13 +82,13 @@ saveRDS(eset1.f, file = 'benchmark/data/multi-omic/eset_GSE117931_methylation_fi
 
 
 # GSE82221
-assay = fread('benchmark/extdata/multi-omic/GSE82221/ADEx_data/methylation.tsv',
+assay = fread('benchmark/extdata/multi-omic/GSE82221/methylation.tsv',
               header = T, data.table = F)
 rownames(assay) = assay[,1]; assay = assay[, -1]
 assay = na.omit(assay)
 assay = as.data.frame(t(apply(assay, 1, function(x) log2(x / (1-x)))))
 meta = read.delim2(
-  'benchmark/extdata/multi-omic/GSE82221/ADEx_data/metadata.tsv',
+  'benchmark/extdata/multi-omic/GSE82221/metadata.tsv',
   header = T)
 meta = meta[match(colnames(assay), meta$Sample),]
 rownames(meta) = meta$Sample

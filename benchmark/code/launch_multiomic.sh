2a179653ef9f3df885841b0826335adcf6382ede
unknown <joselb1993@gmail.com>
2022-06-23 10:34:40 +0200

    update

--- benchmark/code/launch_multiomic.sh
+++ benchmark/code/launch_multiomic.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sbatch /mnt/netapp2/Store_uni/home/ulc/co/jlb/git/Review-MLAID/benchmark/code/launch4.sh
-sbatch /mnt/netapp2/Store_uni/home/ulc/co/jlb/git/Review-MLAID/benchmark/code/launch5.sh
+sbatch /mnt/netapp2/Store_uni/home/ulc/co/jlb/git/Review-MLAID/benchmark/code/launch_4.sh
+sbatch /mnt/netapp2/Store_uni/home/ulc/co/jlb/git/Review-MLAID/benchmark/code/launch_5.sh
--- benchmark/code/launch_singleomic.sh
+++ benchmark/code/launch_singleomic.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sbatch /mnt/netapp2/Store_uni/home/ulc/co/jlb/git/Review-MLAID/benchmark/code/launch1.sh
-sbatch /mnt/netapp2/Store_uni/home/ulc/co/jlb/git/Review-MLAID/benchmark/code/launch2.sh
-sbatch /mnt/netapp2/Store_uni/home/ulc/co/jlb/git/Review-MLAID/benchmark/code/launch3.sh
+sbatch /mnt/netapp2/Store_uni/home/ulc/co/jlb/git/Review-MLAID/benchmark/code/launch_1.sh
+sbatch /mnt/netapp2/Store_uni/home/ulc/co/jlb/git/Review-MLAID/benchmark/code/launch_2.sh
+sbatch /mnt/netapp2/Store_uni/home/ulc/co/jlb/git/Review-MLAID/benchmark/code/launch_3.sh

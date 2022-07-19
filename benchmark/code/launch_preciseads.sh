8e6746ba3aac9b7cb298866c36b2730330ba6dad
jlinaresb <joselb1993@gmail.com>
2022-07-19 11:28:25 +0200

    update

---
+++ benchmark/code/launch_preciseads.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sbatch /mnt/netapp2/Store_uni/home/ulc/co/jlb/git/Review-MLAID/benchmark/code/preciseads/lch_exp.sh
+sbatch /mnt/netapp2/Store_uni/home/ulc/co/jlb/git/Review-MLAID/benchmark/code/preciseads/lch_met.sh
+sbatch /mnt/netapp2/Store_uni/home/ulc/co/jlb/git/Review-MLAID/benchmark/code/preciseads/lch_multi.sh

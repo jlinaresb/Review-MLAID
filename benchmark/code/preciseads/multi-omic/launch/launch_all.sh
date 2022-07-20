7d69add5309d5f5787740195f649ca384efd64f1
jlinaresb <joselb1993@gmail.com>
2022-07-20 10:16:40 +0200

    update

--- benchmark/code/preciseads/multi-omic/launch/launch_all.sh
+++ benchmark/code/preciseads/multi-omic/launch/launch_all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sbatch /home/joselinares/git/Review-MLAID/benchmark/code/preciseads/multi-omic/launch/lch_coca.sh
-sbatch /home/joselinares/git/Review-MLAID/benchmark/code/preciseads/multi-omic/launch/lch_kmeans.sh
-sbatch /home/joselinares/git/Review-MLAID/benchmark/code/preciseads/multi-omic/launch/lch_snf.sh
-sbatch /home/joselinares/git/Review-MLAID/benchmark/code/preciseads/multi-omic/launch/lch_hierarchical.sh
-sbatch /home/joselinares/git/Review-MLAID/benchmark/code/preciseads/multi-omic/launch/lch_mclust.sh
-sbatch /home/joselinares/git/Review-MLAID/benchmark/code/preciseads/multi-omic/launch/lch_som.sh
-sbatch /home/joselinares/git/Review-MLAID/benchmark/code/preciseads/multi-omic/launch/lch_icluster.sh
-sbatch /home/joselinares/git/Review-MLAID/benchmark/code/preciseads/multi-omic/launch/lch_nmf.sh
+sbatch /mnt/netapp2/Store_uni/home/ulc/co/jlb/git/Review-MLAID/benchmark/code/preciseads/multi-omic/launch/lch_coca.sh
+sbatch /mnt/netapp2/Store_uni/home/ulc/co/jlb/git/Review-MLAID/benchmark/code/preciseads/multi-omic/launch/lch_kmeans.sh
+sbatch /mnt/netapp2/Store_uni/home/ulc/co/jlb/git/Review-MLAID/benchmark/code/preciseads/multi-omic/launch/lch_snf.sh
+sbatch /mnt/netapp2/Store_uni/home/ulc/co/jlb/git/Review-MLAID/benchmark/code/preciseads/multi-omic/launch/lch_hierarchical.sh
+sbatch /mnt/netapp2/Store_uni/home/ulc/co/jlb/git/Review-MLAID/benchmark/code/preciseads/multi-omic/launch/lch_mclust.sh
+sbatch /mnt/netapp2/Store_uni/home/ulc/co/jlb/git/Review-MLAID/benchmark/code/preciseads/multi-omic/launch/lch_som.sh
+sbatch /mnt/netapp2/Store_uni/home/ulc/co/jlb/git/Review-MLAID/benchmark/code/preciseads/multi-omic/launch/lch_icluster.sh
+sbatch /mnt/netapp2/Store_uni/home/ulc/co/jlb/git/Review-MLAID/benchmark/code/preciseads/multi-omic/launch/lch_nmf.sh
